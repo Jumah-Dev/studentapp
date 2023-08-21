955cbd856e4b0f47e313747c024ac4fc4e2a8c61
CollyneJumah <collynejumah2010@gmail.com>
2023-08-21 21:19:09 +0300

    project setup

---
+++ includes/navbar.php
@@ -0,0 +1,4 @@
+<div class="header">
+    <img src="images/jumahlogo1.png" alt="logo" height="50" width="50" class="rounded-circle">
+    <a href="#" class="navbar-trigger"><span></span></a>
+</div>
